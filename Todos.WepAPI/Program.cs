39939fdbf2e0c838e83275b2b001a94bf53b642f
FK <FK@FARUKKALKAN>
2025-03-04 18:59:01 +0300

    Create Metodu Eklendi.

--- Todos.WepAPI/Program.cs
+++ Todos.WepAPI/Program.cs
@@ -13,6 +13,9 @@ app.MapGet("/getAll", () => Results.Ok(new List<string>()
     "Example 1",
     "Example 2"
 }));
-
+app.MapGet("/create", (string work) =>
+{
+    Results.Ok(work);
+});
 
 app.Run();
