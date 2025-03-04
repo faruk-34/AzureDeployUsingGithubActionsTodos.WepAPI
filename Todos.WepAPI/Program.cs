3e87ca8590e1950d8886156e2b57c2da95fba928
FK <FK@FARUKKALKAN>
2025-03-04 18:28:06 +0300

    Add project files.

---
+++ Todos.WepAPI/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/getAll", () => Results.Ok(new List<string>()
+{
+    "Example 1",
+    "Example 2"
+}));
+
+
+app.Run();
